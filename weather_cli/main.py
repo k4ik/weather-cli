4c7990ab9cc7ea15dcf5cfb55fa6642bf7e2f0a6
k4ik <kaikleal1234@gmail.com>
2025-06-14 14:16:46 -0300

    fix: update commands

---
+++ weather_cli/main.py
@@ -0,0 +1,68 @@
+import typer
+import time
+from yaspin import yaspin
+import requests
+from dotenv import load_dotenv
+import os
+import json
+
+app = typer.Typer()
+load_dotenv()
+
+def load_translation():
+    lang = os.getenv("TOOL_LANGUAGE", "en")
+    try:
+        with open(f"locales/{lang}.json", "r", encoding="utf-8") as file:
+            return json.load(file)
+    except FileNotFoundError:
+        print(f"Translation file for '{lang}' not found. Falling back to English.")
+        with open("locales/en.json", "r", encoding="utf-8") as file:
+            return json.load(file)
+
+translations = load_translation()
+
+api_key = os.getenv('API_KEY')
+lang = os.getenv('TOOL_LANGUAGE', 'en')
+
+@app.command()
+def weather(city: str):
+    spinner = yaspin()
+    spinner.start()
+    time.sleep(1)
+    spinner.stop()
+
+    res = requests.get(f"http://api.weatherapi.com/v1/current.json?key={api_key}&q={city}&lang={lang}")
+    data = res.json()
+
+    weather_data = {
+        "name": data["location"]["name"],
+        "region": data["location"]["region"],
+        "country": data["location"]["country"],
+        "lat": data["location"]["lat"],
+        "lon": data["location"]["lon"],
+        "localtime": data["location"]["localtime"],
+        "temp": data["current"]["temp_c"],
+        "feels_like": data["current"]["feelslike_c"],
+        "condition": data["current"]["condition"]["text"],
+        "humidity": data["current"]["humidity"],
+        "pressure": data["current"]["pressure_mb"],
+        "wind_speed": data["current"]["wind_kph"],
+        "wind_dir": data["current"]["wind_dir"],
+        "precipitation": data["current"]["precip_mm"],
+        "uv_index": data["current"]["uv"]
+    }
+
+    print("\n")
+    print(translations["weather-info"]["location"].format(**weather_data))
+    print(translations["weather-info"]["localtime"].format(**weather_data))
+    print(translations["weather-info"]["temperature"].format(**weather_data))
+    print(translations["weather-info"]["condition"].format(**weather_data))
+    print(translations["weather-info"]["humidity"].format(**weather_data))
+    print(translations["weather-info"]["pressure"].format(**weather_data))
+    print(translations["weather-info"]["wind"].format(**weather_data))
+    print(translations["weather-info"]["precipitation"].format(**weather_data))
+    print(translations["weather-info"]["uv"].format(**weather_data))
+    print("\n")
+
+if __name__ == "__main__":
+    app()
